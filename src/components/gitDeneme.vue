434422daf7da9365d81375e49bf5509e3f6222a5
macar67 <muhammetacar4522@gmail.com>
2022-01-02 12:34:59 +0300

    cherry-pick yeniden

---
+++ src/components/gitDeneme.vue
@@ -0,0 +1,18 @@
+<template>
+
+    <div className="card m-2" style=" width:18em; background-color:lightgray;">
+   acem kızı acem kızı sarmaşık güllere dolandım
+      one more commit and please smile to me
+      yeni yeni
+
+      deneenendlsnaklfndksjanfkljasnflkjdsjdflkjahyaktın beni bir bakışta  yanıorum yanıyoru gonca güle acem şalı ince bele nail oldum gonca güle acem şalı ince bele
+    </div>
+
+</template>
+<script>
+    export default {
+    name: 'Card',
+    props:['users']
+
+}
+</script>
